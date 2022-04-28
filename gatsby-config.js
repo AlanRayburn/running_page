8824770886a9072b5e1687e4f5739014d2299046
AlanRayburn <alanrayburn85@gmail.com>
2022-04-28 11:45:11 +0800

    firstcommit

---
+++ gatsby-config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  pathPrefix: `/running_page` , // Change to `/running_page` when running on github pages
+  siteMetadata: {
+    siteTitle: 'Rayburn Running ',
+    siteUrl: 'https://run.notion.vip',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    description: 'Personal site and blog',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://notion.rayburn.vip/',
+      },
+      {
+        name: 'About',
+        url: 'https://github.com/AlanRayburn/running_page',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-responsive-iframe',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-widows',
+          'gatsby-remark-external-links',
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              className: 'header-link',
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+  ],
+};
